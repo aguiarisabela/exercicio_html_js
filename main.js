0699fa6aa48870d841a81da0cd556e7b6c067a7e
Isabela Aguiar <isilav.is54@gmail.com>
2024-09-20 12:07:46 -0300

    Corrigindo erros

--- main.js
+++ main.js
@@ -1,27 +1,35 @@
-const numeroA = Number(document.getElementById ('numero-a').value)
-const numeroB = Number(document.getElementById('numero-b').value)
+const form = document.getElementById('form')
 const calcular = document.getElementById('botao-enviar')
-const mensagemErro = `Formulário inválido! O número ${numeroB.value} deve ser maior que o ${numeroA.value}.`
-const mensagemSucesso = `Formulário válido! O ${numeroB.value} é maior que o ${numeroA.value}.`
 
-
-function calcular (numero){
-
-    numero= numeroB.value > numeroA.value
-    return numero
-}
-form.addEventListener('subimit', function(e){
+form.addEventListener('submit', function(e){
     e.preventDefault()
 
+const numeroA = Number(document.getElementById ('numero-a').value)
+const numeroB = Number(document.getElementById('numero-b').value)
+
+const mensagemErro = `Formulário inválido! O número ${numeroB} deve ser maior que o ${numeroA}.`
+const mensagemSucesso = `Formulário válido! O ${numeroB} é maior que o ${numeroA}.`
+
+function calcular (){
+
+    return numeroB > numeroA
+    
+}
 
 if(calcular()){ 
     const containerMensagemSucesso = document.querySelector('#mensagem-sucesso')
        containerMensagemSucesso.innerHTML = mensagemSucesso
        containerMensagemSucesso.style.display = 'block'
+
+       const containerMensagemErro = document.querySelector('#mensagem-erro');
+        containerMensagemErro.style.display = 'none';
    
    } else{
     const containerMensagemErro = document.querySelector('#mensagem-erro')
        containerMensagemErro.innerHTML = mensagemErro
        containerMensagemErro.style.display = 'block'
+
+       const containerMensagemSucesso = document.querySelector('#mensagem-sucesso');
+        containerMensagemSucesso.style.display = 'none';
    }
    })
